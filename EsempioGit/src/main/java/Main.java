ff2869bbc5a2e2a291ae478cf9a1be09346783c0
Pasquale Foggia <pfoggia@unisa.it>
2021-11-09 10:43:47 +0100

    Modified version of Main

--- EsempioGit/src/main/java/Main.java
+++ EsempioGit/src/main/java/Main.java
@@ -6,7 +6,7 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("Concurrent modification 1");
-        System.out.println("Hello, world");
+        System.out.println("Hello, world A");
         System.out.println("Modified version");
         System.out.println("Concurrent modification 2");
     }
