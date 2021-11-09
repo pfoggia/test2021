91f1c85f1fdd723848bc5915325c25a238467ef5
Pasquale Foggia <pfoggia@unisa.it>
2021-11-09 10:56:24 +0100

    Cleaned up version

--- EsempioGit/src/main/java/Main.java
+++ EsempioGit/src/main/java/Main.java
@@ -5,9 +5,6 @@
  */
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Concurrent modification 1");
         System.out.println("Hello, world AB");
-        System.out.println("Modified version");
-        System.out.println("Concurrent modification 2");
     }
 }
